cfbee4d79ed99b398c654de16f111b01587dd9f9
liyan64 <liyan64@meituan.com>
2018-06-24 00:05:26 +0800

    modify bugs(sb mongodb)

--- SJR_mongodb.py
+++ SJR_mongodb.py
@@ -26,16 +26,21 @@ class SJR_mongodb(object):
     def insert_journal_collection(self, journal_dict):
         my_collection = self.database[JOURNAL_COLLECTION]
         insert_item = {}
-        for jour, cate_list in journal_dict:
+        for jour, cate_list in journal_dict.items():
             insert_item['journal'] = jour
             insert_item['subject_area'] = SUBJECT_AREA
             insert_item['subject_categories'] = cate_list
-            insert_id = my_collection.insert(insert_item)
+            insert_id = my_collection.save(insert_item)
+            # 重点：清空字典
+            # 报错：E11000 duplicate key error collection
+            # 原因：共用一个外部变量，mongodb认为是同一条数据，太sb了
+            insert_item.clear()
             logger_info.info('{} insert success!'.format(insert_id))
 
     def match_journal_area_categories(self, journal_dict):
         match_collection = self.database[MATCH_COLLECTION]
         insert_item = {}
+        counter = 0
         for i in range(9):
             collection = self.database["mag_papers_" + str(i)]
             cursor = collection.find()
@@ -51,6 +56,9 @@ class SJR_mongodb(object):
                             insert_item['subject_area'] = SUBJECT_AREA
                             insert_item['subject_categories'] = journal_dict[journal]
                             match_id = match_collection.insert(insert_item)
+                            counter = counter + 1
+                            if counter >= len(journal_dict):
+                                break
                             logger_info.info('{} insert success!'.format(match_id))
                     else:
                         logger_info.error('{} has no venue!'.format(doc['_id']))
